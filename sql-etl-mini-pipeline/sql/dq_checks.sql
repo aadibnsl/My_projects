6ebd56c11bb577fa907a0425610308e31efb5572
aadibnsl <aadityabansal26@gmail.com>
2025-12-13 19:33:35 +0530

    Add data quality views for employees and attendance

---
+++ sql-etl-mini-pipeline/sql/dq_checks.sql
@@ -0,0 +1,17 @@
+############################# DQ VIEWS ###################################
+
+CREATE OR REPLACE VIEW dq_duplicate_employees AS
+SELECT emp_id, COUNT(*) AS cnt
+FROM stg_employees
+GROUP BY emp_id
+HAVING COUNT(*) > 1;
+
+CREATE OR REPLACE VIEW dq_missing_attendance AS
+SELECT emp_id, attendance_date
+FROM stg_attendance
+WHERE status NOT IN ('P','A');
+
+CREATE OR REPLACE VIEW dq_invalid_salary AS
+SELECT *
+FROM stg_salaries
+WHERE salary <= 0;
